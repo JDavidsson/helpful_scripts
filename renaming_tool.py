63db1ddc4220c91dd5aa706b1fbdc0dd0e03ed62
JDavidsson <johan.et.davidsson@gmail.com>
2016-12-26 03:01:51 +0100

    remove some duplicate code, fixed windows/mac bug

--- renaming_tool.py
+++ renaming_tool.py
@@ -2,46 +2,20 @@
 import argparse, os, sys
 
 if sys.version_info <= (3, 0):
-    print("python 3.x required")
+    print("sry, python 3.x required")
     exit(1)
 
-def preview(run_for_files, run_for_folders, path, desired_depth, lowercase, uppercase, replacements):
-    print('\n --- preview ---\n')
-    for root, dirnames, files in os.walk(path, topdown=True):
-        current_depth = root.count(os.sep) - path.count(os.sep)
-        if current_depth > desired_depth:
-            break
+def main(run_for_files, run_for_folders, path, desired_depth, lowercase, uppercase, replacements, phase):
 
-        if run_for_folders:
-            for dirname in dirnames:
-                    new = dirname
-                    if lowercase:
-                        new = dirname.lower()
-                    elif uppercase:
-                        new = dirname.upper()
-                    for r in replacements:
-                        new = new.replace(r[0], r[1])
-                    print(os.path.join(root, dirname), '>>', new)
-
-        if run_for_files:
-            for file in files:
-                # remove file extension before manipulation
-                new, ext = os.path.splitext(file)[0], os.path.splitext(file)[1]
-                if lowercase:
-                    new = new.lower()
-                elif uppercase:
-                    new = new.upper()
-                for r in replacements:
-                    new = new.replace(r[0], r[1])
-                    new += ext
-                print(os.path.join(root, file), '>>', new)
-
-    if not run_for_files and not run_for_folders:
-        print("nothing to preview, please select --files, --folders or both")
+    if not os.path.exists(args.p[0]):
+        print('\ncannot find path...\n')
         exit(1)
+    if phase == 1:
+        print('\n --- preview (phase 1) ---\n')
+    if phase == 2:
+        print('\n --- execute changes (phase 2) ---\n')
+        errors = 0
 
-def execute_change(run_for_files, run_for_folders, path, desired_depth, lowercase, uppercase, replacements):
-    errors = 0
     for root, dirnames, files in os.walk(path, topdown=False):
         current_depth = root.count(os.sep) - path.count(os.sep)
         if not current_depth > desired_depth:
@@ -55,11 +29,13 @@ def execute_change(run_for_files, run_for_folders, path, desired_depth, lowercas
                     for r in replacements:
                         new = new.replace(r[0], r[1])
                     print(os.path.join(root, dirname), '>>', new)
-                    try:
-                        os.rename(os.path.join(root, dirname), root + '\\' + new)
-                    except PermissionError as p:
-                        print('Permission denied,', p)
-                        errors += 1
+
+                    if phase == 2:
+                        try:
+                            os.rename(os.path.join(root, dirname), os.path.join(root, new))
+                        except PermissionError as p:
+                            print('Permission denied,', p)
+                            errors += 1
 
             if run_for_files:
                 for file in files:
@@ -73,12 +49,18 @@ def execute_change(run_for_files, run_for_folders, path, desired_depth, lowercas
                         new = new.replace(r[0], r[1])
                         new += ext
                     print(os.path.join(root, file), '>>', new)
-                    try:
-                        os.rename(os.path.join(root, file), root + '\\' + new)
-                    except PermissionError as p:
-                        print('Permission denied,', p)
-                        errors += 1
-    print('\nfinished with', errors, 'error(s)')
+
+                    if phase == 2:
+                        try:
+                            os.rename(os.path.join(root, dirname), os.path.join(root, new))
+                        except PermissionError as p:
+                            print('Permission denied,', p)
+                            errors += 1
+
+    if not run_for_files and not run_for_folders:
+        print("nothing to preview, please select --files, --folders or both")
+        exit(1)
+    if phase ==2: print('\nfinished with', errors, 'error(s)')
 
 parser = argparse.ArgumentParser(description='renaming multiple folders and/or files at once', epilog='phase 1: preview changes \nphase 2: make changes\n ', formatter_class=argparse.RawTextHelpFormatter)
 parser.add_argument('--files', action='store_true', help='run for files')
@@ -93,12 +75,8 @@ args = parser.parse_args()
 replacements = list()
 replacements.append(args.r)
 
-if not os.path.exists(args.p[0]):
-    print('\ncannot find path...\n')
-    exit(1)
-
-preview(args.files, args.folders, args.p[0], args.d, args.l, args.U, replacements)
+main(args.files, args.folders, args.p[0], args.d[0], args.l, args.U, replacements, 1)
 
 q = input('\nAre you sure you want to do this? \nType \'yes\' or \'no\': ')
 if q == 'yes':
-    execute_change(args.files, args.folders, args.p[0], args.d, args.l, args.U, replacements)
+    main(args.files, args.folders, args.p[0], args.d[0], args.l, args.U, replacements, 2)
